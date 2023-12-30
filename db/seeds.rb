1ad24b4238f7e24266505abcdf634f3a7f5a9da1
Harry <matheusthebr@gmail.com>
2023-12-30 11:28:04 +0000

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+customer = Customer.create(email: 'customer@email.com', password: 'TESTp@ss0rd')
+
+statement = Statement.create(customer: customer)
+StatementItem.create(
+  title: 'salary', amount: 2800.0, transaction_type: 'income', statement: statement
+)
+
+StatementItem.create(
+  title: 'other', amount: 300.0, transaction_type: 'income', statement: statement
+)
+
+StatementItem.create(
+  title: 'Mortgage', amount: 500.0, transaction_type: 'expenditure', statement: statement
+)
+
+StatementItem.create(
+  title: 'Utilities', amount: 100.0, transaction_type: 'expenditure', statement: statement
+)
+
+StatementItem.create(
+  title: 'Travel', amount: 150.0, transaction_type: 'expenditure', statement: statement
+)
+
+StatementItem.create(
+  title: 'Food', amount: 500.0, transaction_type: 'expenditure', statement: statement
+)
+
+StatementItem.create(
+  title: 'Loan Repayment', amount: 1000.0, transaction_type: 'expenditure', statement: statement
+)
